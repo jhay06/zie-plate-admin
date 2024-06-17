68ece13a58d72047be567d70bb2ad3ac87705297
jhay mendoza <jhaymendoza@192.168.1.4>
2024-06-17 20:40:43 +0800

    notiication user

--- zie-plate-admin/main.py
+++ zie-plate-admin/main.py
@@ -3,24 +3,29 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-@app.route('/notification/user', methods=['POST','GET'])
+@app.route('/notification/user', methods=['POST', 'GET'])
 def notificate_user():
     return "Noticication for User"
+
 
 @app.route('/auth/token_verify', methods=['POST'])
 def token_verify():
     data = request.get_json()
     print(data)
     return jsonify(data)
+
+
 @app.route('/api', methods=['POST'])
 def api():
     data = request.get_json()
     print(data)
     return jsonify(data)
 
+
 @app.route('/')
 def index():
     return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
