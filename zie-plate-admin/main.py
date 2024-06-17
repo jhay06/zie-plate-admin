cee6b2d7e4460101bc59d0a16a9fa109a41db1e7
jhay mendoza <jhaymendoza@192.168.1.4>
2024-06-17 20:29:09 +0800

    added notificate user

--- zie-plate-admin/main.py
+++ zie-plate-admin/main.py
@@ -2,6 +2,18 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+
+@app.route('/notification/user', methods=['POST'])
+def notificate_user():
+    data = request.get_json()
+    print(data)
+    return jsonify(data)
+
+@app.route('/auth/token_verify', methods=['POST'])
+def token_verify():
+    data = request.get_json()
+    print(data)
+    return jsonify(data)
 @app.route('/api', methods=['POST'])
 def api():
     data = request.get_json()
